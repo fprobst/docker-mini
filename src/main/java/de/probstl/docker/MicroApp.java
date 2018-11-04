aece4a3bb754833292f932fda6403b579686fd0a
fprobst <flo@probstl.de>
2018-11-04 20:20:18 +0100

    Added spring classes for REST webservice

---
+++ src/main/java/de/probstl/docker/MicroApp.java
@@ -0,0 +1,13 @@
+package de.probstl.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroApp.class, args);
+	}
+	
+}
